f0f6db9310f98b3d0cd2bcdcc54a7d36cd13c62e
Louise <louiseannesuriobechayda@gmail.com>
2026-02-12 21:53:27 +0800

    Remove react-native-ui-n-interaction from repo

--- react-native-ui-n-interaction/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import LotsOfStyles from './FixedDimension'; 
-import FixedDimensionsBasics from './FixedDimension';
-
-export default function App() {
-  return <FixedDimensionsBasics />;  
-}
