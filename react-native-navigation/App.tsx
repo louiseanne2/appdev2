fa65b1d3998b90edfb689a3ad21171ef27f0fcab
Louise <louiseannesuriobechayda@gmail.com>
2026-04-20 12:22:43 +0800

    Imported and configured TypeScript in App file

---
+++ react-native-navigation/App.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
+
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { RouteProp} from '@react-navigation/native'
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    itemId: number;
+    otherParam?: string;
+  };
+};
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+
+type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
+
+type DetailsProps = {
+  route: DetailsScreenRouteProp;
+};
+
+function HomeScreen() {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      >
+        Go to Details
+      </Button>
+    </View>
+  );
+}
+
+
+function DetailsScreen({ route }: DetailsProps) {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        onPress={
+          () =>
+            navigation.push('Details', {
+              // Randomly generate an ID for demonstration purposes
+              itemId: Math.floor(Math.random() * 100),
+            })
+        }
+      >
+        Go to Details... again
+      </Button>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator<RootStackParamList>({
+  screens: {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
