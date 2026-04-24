b5e4f47446dc622194acb0e33779d5782354b6d9
Louise <louiseannesuriobechayda@gmail.com>
2026-04-24 15:06:31 +0800

    Done with create the todo API(CRUD)

--- react-native-todo-convex/convex/_generated/api.d.ts
+++ react-native-todo-convex/convex/_generated/api.d.ts
@@ -8,13 +8,17 @@
  * @module
  */
 
+import type * as todo from "../todo.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  todo: typeof todo;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
